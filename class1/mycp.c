db0c771c93207e816aff6d36d8f946f7fc213e85
Jose Vasconcelos <zevasconcelos6@gmail.com>
2025-02-11 18:10:27 +0000

    Medidor de tempo do mycp.c

--- class1/mycp.c
+++ class1/mycp.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #define SIZE 4096
 
@@ -22,10 +23,16 @@ int main(int argc, char** argv){
         // ./mycp [SOURCE] [DESTINATION]
         int fdSrc = open(argv[1], O_RDONLY);
         int fdDest= open(destPath, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+        clock_t start = clock();
         ssize_t bytesRead = 0;
+
         while( (bytesRead = read(fdSrc, buffer, SIZE)) > 0){
             write(fdDest, buffer, bytesRead);
         }
+        clock_t end = clock();
+        double elapsed = ((double)(end - start)) / CLOCKS_PER_SEC;
+    
+        printf("Tempo total de cópia: %.6f segundos\n", elapsed);
         close(fdSrc);
         close(fdDest);
         
