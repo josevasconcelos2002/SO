705237b69f32c6be4a8a736ee389d8a1a14fa4ed
Jose Vasconcelos <zevasconcelos6@gmail.com>
2025-03-27 08:40:48 +0000

    Ficheiro de logs

--- guiao5/ex2_skeleton/server.c
+++ guiao5/ex2_skeleton/server.c
@@ -18,19 +18,29 @@ int main (int argc, char * argv[]) {
     int n;
     Msg mensagem;
 
+
     while (1) {
 
             n = read(request_fifo, &mensagem, sizeof(Msg));
 
+
             if(n>0){
+
+                int logs = open("logs.txt", O_CREAT | O_WRONLY | O_APPEND, 0666);
 
                 printf("\nMensagem recebida:\n");
                 printf("Needle: %d\n", mensagem.needle);
                 printf("PID: %d\n", mensagem.pid);
                 printf("Occurrences: %d\n", mensagem.occurrences);
-
                 
                 mensagem.occurrences = count_needle(mensagem.needle);
+
+                char buffer[128];
+                int len = snprintf(buffer, sizeof(buffer),
+                   "Needle: %d\nPID: %d\nOccurrences: %d\n\n",
+                   mensagem.needle, mensagem.pid, mensagem.occurrences);
+
+                write(logs, buffer, len);
 
                 char *response_fifo_name = malloc(128);
                 sprintf(response_fifo_name, "../fifos/response_fifo_%d", mensagem.pid);
@@ -43,6 +53,7 @@ int main (int argc, char * argv[]) {
                 printf("Occurrences: %d\n", mensagem.occurrences);
                 
                 free(response_fifo_name);
+                close(logs);
                 close(response_fifo);
             }
             
